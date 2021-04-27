a6f2cea442569b4bdc80410e7de39fe8bc8fe025
Gustav Sörnäs <gustav@sornas.net>
2021-04-28 00:09:41 +0200

    initial game struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,75 @@
+use luminance_sdl2::sdl2;
+use luminance_sdl2::GL33Surface;
+use sdl2::audio::AudioDevice;
+use sdl2::Sdl;
+use std::time::Instant;
+
 pub mod audio;
 pub mod asset;
 pub mod input;
 pub mod random;
 pub mod renderer;
 pub mod performance;
+
+/// Everything you need to create a game.
+pub struct Game {
+    pub audio: AudioDevice<audio::Audio>,
+    pub assets: asset::AssetSystem,
+    pub renderer: renderer::Renderer,
+
+    surface: GL33Surface,
+    start_t: Instant,
+    prev_t: f32,
+}
+
+impl Game {
+    pub fn new(title: &str, window_width: u32, window_height: u32) -> Self {
+        let mut surface = GL33Surface::build_with(|video| video.window(title,
+                                                                       window_width,
+                                                                       window_height))
+            .expect("Failed to create surface");
+
+        let mut sampler = luminance::texture::Sampler::default();
+        sampler.mag_filter = luminance::texture::MagFilter::Nearest;
+        let renderer = renderer::Renderer::new(&mut surface, sampler);
+
+        let audio = audio::Audio::init(surface.sdl());
+        audio.resume();
+        let assets = asset::AssetSystem::new();
+
+        Self {
+            audio,
+            assets,
+            renderer,
+
+            surface,
+            start_t: Instant::now(),
+            prev_t: 0.0,
+        }
+    }
+    
+    pub fn sdl(&self) -> &Sdl {
+        self.surface.sdl()
+    }
+
+    pub fn time_tick(&mut self) -> f32 {
+        let t = self.start_t.elapsed().as_millis() as f32 * 1e-3;
+        let delta = t - self.prev_t;
+        self.prev_t = t;
+        delta
+    }
+
+    pub fn total_time(&self) -> f32 {
+        self.prev_t
+    }
+
+    pub fn update(&mut self, _delta: f32) { 
+        performance::frame();
+        self.assets.reload();
+        self.renderer.reload();
+    }
+
+    pub fn draw(&mut self) -> Result<(), ()> {
+        self.renderer.render(&mut self.surface)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
-use luminance_sdl2::GL33Surface;
 use std::f32::consts::PI;
 use std::path::Path;
-use std::time::Instant;
 
-use lingon::audio::Audio;
-use lingon::asset::AssetSystem;
 use lingon::input;
 use lingon::random::{self, Distribute, RandomProperty};
-use lingon::performance;
+use lingon::renderer::{ParticleSystem, Rect, Sprite, Transform};
 
 /// A list of all valid inputs.
 #[derive(Copy, Clone, Debug, Eq, Hash, PartialEq)]
@@ -21,22 +17,23 @@ pub enum Name {
 }
 
 fn main() {
-    let surface = GL33Surface::build_with(|video| video.window("game", 800, 600))
-        .expect("Failed to create surface");
+    let mut game = lingon::Game::new("game", 800, 600);
+    
+    let mut input = input::InputManager::new(game.sdl());
+    input.bind(input::Device::Key(input::Keycode::A), Name::Left);
+    input.bind(input::Device::Key(input::Keycode::D), Name::Right);
+    input.bind(input::Device::Key(input::Keycode::W), Name::Up);
+    input.bind(input::Device::Key(input::Keycode::S), Name::Down);
+    input.bind(input::Device::Key(input::Keycode::Escape), Name::Quit);
+    input.bind(input::Device::Key(input::Keycode::F), Name::PlaySound);
+    input.bind(input::Device::Quit, Name::Quit);
+    input.bind(input::Device::Axis(0, input::Axis::LeftX), Name::Right);
+    input.bind(input::Device::Axis(0, input::Axis::RightY), Name::Up);
 
-    main_loop(surface);
-}
+    let image = game.assets.load_image(Path::new("res/coin-gold.png").to_path_buf());
+    let bloop = game.assets.load_audio(Path::new("res/bloop.wav").to_path_buf());
 
-fn main_loop(mut surface: GL33Surface) {
-    use lingon::renderer::*;
-    let mut sampler = luminance::texture::Sampler::default();
-    sampler.mag_filter = luminance::texture::MagFilter::Nearest;
-    let mut renderer = Renderer::new(&mut surface, sampler);
-
-    let mut assets = AssetSystem::new();
-    let image = assets.load_image(Path::new("res/coin-gold.png").to_path_buf());
-
-    let sheet = renderer.add_sprite_sheet(assets[image].clone(), (16, 16));
+    let sheet = game.renderer.add_sprite_sheet(game.assets[image].clone(), (16, 16));
 
     let mut particle_system = lingon::particle_system!(
         lifetime       = [1.0, 2.0]    random::TwoDice,
@@ -53,60 +50,43 @@ fn main_loop(mut surface: GL33Surface) {
         angle_drag     = [0.0, 2.0]    random::TwoDice,
     );
 
-    let start_t = Instant::now();
+    'main: loop {
+        let delta = game.time_tick();
+        game.update(delta);
 
-    let mut input = input::InputManager::new(surface.sdl());
-    input.bind(input::Device::Key(input::Keycode::A), Name::Left);
-    input.bind(input::Device::Key(input::Keycode::D), Name::Right);
-    input.bind(input::Device::Key(input::Keycode::W), Name::Up);
-    input.bind(input::Device::Key(input::Keycode::S), Name::Down);
-    input.bind(input::Device::Key(input::Keycode::Escape), Name::Quit);
-    input.bind(input::Device::Key(input::Keycode::F), Name::PlaySound);
-    input.bind(input::Device::Quit, Name::Quit);
-    input.bind(input::Device::Axis(0, input::Axis::LeftX), Name::Right);
-    input.bind(input::Device::Axis(0, input::Axis::RightY), Name::Up);
-
-    let mut audio = Audio::init(surface.sdl());
-    audio.resume();
-
-    let bloop = assets.load_audio(Path::new("res/bloop.wav").to_path_buf());
-
-    let mut old_t = start_t.elapsed().as_millis() as f32 * 1e-3;
-    'app: loop {
-        let t = start_t.elapsed().as_millis() as f32 * 1e-3;
-        let delta = t - old_t;
-        old_t = t;
-
-        performance::frame();
-
+        // poll input
         let timer = lingon::counter!("input");
-        input.poll(surface.sdl());
+        input.poll(game.sdl());
         drop(timer);
 
         if input.pressed(Name::Quit) {
-            break 'app;
+            break 'main;
         }
 
         if input.pressed(Name::PlaySound) {
-            audio.lock().play(&assets[bloop]);
+            game.audio.lock().play(&game.assets[bloop]);
         }
 
-        particle_system.position[0] = t.cos() * 0.5;
-        particle_system.position[1] = t.sin() * 0.5;
+        // update particle system
+        particle_system.position[0] = game.total_time().cos() * 0.5;
+        particle_system.position[1] = game.total_time().sin() * 0.5;
         for _ in 0..5 {
             particle_system.spawn();
         }
         particle_system.update(delta);
 
-        let region =
-            renderer.sprite_sheets[sheet].grid([0, 1, 2, 3, 2, 1][((t * 10.0) as usize) % 6], 0);
+        let region = game.renderer.sprite_sheets[sheet].grid(
+                [0, 1, 2, 3, 2, 1][((game.total_time() * 10.0) as usize) % 6],
+                0
+        );
+
         for x in -5..5 {
             for y in -5..5 {
-                renderer.push(
+                game.renderer.push(
                     Sprite::new(region)
                         .at(x as f32, y as f32)
                         .scale(0.3, 0.3)
-                        .angle(t),
+                        .angle(game.total_time()),
                 );
             }
         }
@@ -121,21 +101,17 @@ fn main_loop(mut surface: GL33Surface) {
         for (i, v) in buckets.iter().enumerate() {
             let w = 1.0 / (NUM_BUCKETS as f32);
             let h = (*v as f32) * w * 0.1;
-            renderer.push(Rect::new().scale(w, h).at((i as f32) * w, h / 2.0));
+            game.renderer.push(Rect::new().scale(w, h).at((i as f32) * w, h / 2.0));
         }
 
-        renderer.push_particle_system(&particle_system);
+        game.renderer.push_particle_system(&particle_system);
         //input.rumble(0, input.value(input::Name::Right), input.value(input::Name::Up), 1.0).unwrap();
-        renderer.camera.move_by(
+        game.renderer.camera.move_by(
             (input.value(Name::Right) - input.value(Name::Left)) * delta,
             (input.value(Name::Up) - input.value(Name::Down)) * delta,
         );
-
-        assets.reload();
-        renderer.reload();
-
-        if renderer.render(&mut surface).is_err() {
-            break 'app;
+        if game.draw().is_err() {
+            break 'main;
         }
     }
 }
