480f6bf73a2573409072970d3f09bde00eaa8ae5
Edvard Thörnros <edvard.thornros@gmail.com>
2021-04-27 21:45:48 +0200

    Change to atomics

--- lingon_macro/src/lib.rs
+++ lingon_macro/src/lib.rs
@@ -1,18 +1,14 @@
 use lazy_static::lazy_static;
-use std::sync::{Arc, Mutex};
+use std::sync::atomic::{AtomicUsize, Ordering};
 use quote::quote;
 
-
 lazy_static! {
-    static ref PERF_COUNTER: Arc<Mutex<usize>> = Arc::new(Mutex::new(0));
+    static ref PERF_COUNTER: AtomicUsize = AtomicUsize::new(0);
 }
 
 #[proc_macro]
 pub fn perf_counter(_: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let mut counter = PERF_COUNTER.lock().unwrap();
-    *counter = (*counter) + 1;
-    let value = *counter;
-
+    let value = PERF_COUNTER.fetch_add(1, Ordering::Relaxed);
     let tokens = quote! {
         #value
     };
