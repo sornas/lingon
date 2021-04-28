8003c7f5bedd6a052e5da8f83a270faa9e4e3722
Gustav Sörnäs <gustav@sornas.net>
2021-04-28 19:15:18 +0200

    Revert "=====REMOVE ME===== add thoughts"
    This reverts commit 6367197436a2b7fb8056da50fe0815567e191c1b.

--- src/main.rs
+++ src/main.rs
@@ -38,18 +38,13 @@ fn main() {
     // Create the initial game state and input manager.
     let mut game = lingon::Game::new("game", 800, 600);
     let mut input = bind_inputs(game.sdl());
-    //XXX The input manager is completely separate since I don't want to store it on the struct,
-    //but it still feels weird.
 
     // Load an image and a sound.
     let coin = game.assets.load_image(Path::new("res/coin-gold.png").to_path_buf());
     let bloop = game.assets.load_audio(Path::new("res/bloop.wav").to_path_buf());
-    //XXX game.load_image("res/coin-gold.png") and game.load_audio("res/bloop.wav")
 
     // Add our image as a sprite sheet.
     let coin_sheet = game.renderer.add_sprite_sheet(game.assets[coin].clone(), (16, 16));
-    //XXX I feel like load_image should do this directly. Unless we need images that aren't sprite
-    //sheets somewhere else.
 
     // Create a particle system.
     let mut particle_system = lingon::particle_system!(
@@ -71,28 +66,6 @@ fn main() {
         // Go a step forward in time.
         let delta = game.time_tick();
         game.update(delta);
-        //XXX Here we could choose to go the other way and have our game struct take an update and
-        //draw function. But what happens with global state then? Maybe we can do:
-        /*
-         * pub struct Game<STATE> {
-         *     // ...
-         *     state: STATE,
-         *     update: fn(&mut STATE, f32),
-         *     draw: fn(&mut STATE),
-         * }
-         *
-         * impl<S> Game<S> {
-         *     fn run(&mut self) {
-         *         loop {
-         *             // ...
-         *             update(&mut self.state, delta);
-         *             // ...
-         *             draw(&mut self.state);
-         *             // ...
-         *         }
-         *     }
-         * }
-         */
 
         // Poll input and time it.
         let timer = lingon::counter!("input");
@@ -106,7 +79,6 @@ fn main() {
         if input.pressed(Name::PlaySound) {
             // Play an audio asset.
             game.audio.lock().play(&game.assets[bloop]);
-            //XXX I kinda agree that this should be game.play_audio(&game.assets[bloop])
         }
 
         // Move the particle system in a circle. One revolution takes 2*PI seconds.
@@ -137,8 +109,6 @@ fn main() {
                         .scale(0.3, 0.3)
                         // Also, spin them around! :D
                         .angle(game.total_time()),
-                        //XXX Unsure about builder pattern for "at" here. But it works woders for
-                        //scale and angle!
                 );
             }
         }
@@ -156,20 +126,16 @@ fn main() {
             let w = 1.0 / (NUM_BUCKETS as f32);
             let h = (*v as f32) * w * 0.1;
             game.renderer.push(Rect::new().scale(w, h).at((i as f32) * w, h / 2.0));
-            //XXX Same here. I'm unsure about builder for w,h and x,y since they should always be
-            //passed.
         }
 
         // Tell the renderer to draw the particle system.
         game.renderer.push_particle_system(&particle_system);
-        //XXX game.renderer.push(&particle_system), ideally. More traits tho :S
 
         // Tell the renderer to move the camera.
         game.renderer.camera.move_by(
             (input.value(Name::Right) - input.value(Name::Left)) * delta,
             (input.value(Name::Up) - input.value(Name::Down)) * delta,
         );
-        //XXX game.move_camera_by(...)
 
         // Draw this frame.
         if game.draw().is_err() {
